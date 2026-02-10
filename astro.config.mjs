e2988ed3a5e6001bc411691c42f32f33d34da338
Mattan Ingram <mattaningram@gmail.com>
2026-02-10 16:29:42 -0500

    Move content from mattaningram.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,10 @@ import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    responsiveStyles: true,
+  },
+
   vite: {
     plugins: [tailwindcss()],
     css: {
